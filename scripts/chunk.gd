403df48944fe7e2ad935208cb73674f9f481f366
gogozoomer@outlook.com <gogozoomer@outlook.com>
2025-06-01 16:55:22 -0700

    v1.9.3

--- scripts/chunk.gd
+++ scripts/chunk.gd
@@ -1,118 +1,161 @@
 extends Node3D
 class_name Chunk
 
-static var active_threads: int = 0
-static var thread_mutex: Mutex = Mutex.new()
 const MAX_THREADS: int = 128
 const LOD_DIVISIONS: int = 4
 const TEX_TILE_SIZE: float = 1.0
 
-var meshInstance: MeshInstance3D
-var collisionShape: CollisionShape3D
-var noise: FastNoiseLite
+static var thread_semaphore: Semaphore = Semaphore.new()
+static var _semaphore_init: bool = false
+
+var noise_template: FastNoiseLite
 var chunkX: int
 var chunkZ: int
 var chunkSize: int
-var thread: Thread = null
+var height_amp: float
 var ground_material: Material
-var A: float
+
+var meshInstance: MeshInstance3D
+var collisionShape: CollisionShape3D
+var thread: Thread
 
 func _init(noiseParam: FastNoiseLite, x: int, z: int, size: int, height: float = 10.0) -> void:
-	noise = noiseParam
+	noise_template = noiseParam
 	chunkX = x
 	chunkZ = z
 	chunkSize = size
-	A = height
+	height_amp = height
 
 func _ready() -> void:
-	if meshInstance == null:
-		meshInstance = MeshInstance3D.new()
-		meshInstance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_ON
-		add_child(meshInstance)
-	if collisionShape == null:
-		collisionShape = CollisionShape3D.new()
-		add_child(collisionShape)
-	_try_launch_thread()
+	if not _semaphore_init:
+		for _i in range(MAX_THREADS):
+			thread_semaphore.post()
+		_semaphore_init = true
+	
+	meshInstance = MeshInstance3D.new()
+	meshInstance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_ON
+	add_child(meshInstance)
+	
+	collisionShape = CollisionShape3D.new()
+	add_child(collisionShape)
+	
+	_spawn_build_thread()
 
-func _try_launch_thread() -> void:
-	thread_mutex.lock()
-	while active_threads >= MAX_THREADS:
-		thread_mutex.unlock()
-		await get_tree().create_timer(0.125).timeout
-		if not is_inside_tree():
-			return
-		thread_mutex.lock()
-	thread_mutex.unlock()
+func _exit_tree() -> void:
+	if thread and thread.is_alive():
+		thread.wait_to_finish()
+		thread = null
+		thread_semaphore.post()
+
+func _spawn_build_thread() -> void:
+	if not thread_semaphore.try_wait():
+		call_deferred("_spawn_build_thread")
+		return
 	thread = Thread.new()
 	var err: int = thread.start(Callable(self, "_threaded_generate_chunk"))
 	if err != OK:
-		push_error("Failed to start terrain generation thread: %s" % err)
-	else:
-		thread_mutex.lock()
-		active_threads += 1
-		thread_mutex.unlock()
-
-func _threaded_generate_chunk() -> void:
-	var meshObj: ArrayMesh = _build_mesh_fast()
-	call_deferred("_apply_mesh", meshObj)
-	call_deferred("_cleanup_thread")
+		push_error("Failed to start terrain thread: %s" % err)
+		thread_semaphore.post()
 
 func _cleanup_thread() -> void:
 	if thread:
 		thread.wait_to_finish()
 		thread = null
-		thread_mutex.lock()
-		active_threads = max(0, active_threads - 1)
-		thread_mutex.unlock()
+	thread_semaphore.post()
 
-func _build_mesh_fast() -> ArrayMesh:
+func _threaded_generate_chunk() -> void:
+	var local_noise: FastNoiseLite = noise_template.duplicate()
+	var mesh: ArrayMesh = _build_mesh_fast(local_noise)
+	call_deferred("_apply_mesh", mesh)
+	call_deferred("_cleanup_thread")
+
+func _build_mesh_fast(n: FastNoiseLite) -> ArrayMesh:
 	@warning_ignore("integer_division")
-	var div: int = chunkSize / LOD_DIVISIONS
-	div = max(div, 1)
+	var div: int = max(chunkSize / LOD_DIVISIONS, 1)
 	var size: int = div + 1
 	var inv_div: float = 1.0 / float(div)
 	var step: float = float(chunkSize) * inv_div
 	var half: float = float(chunkSize) * 0.5
-
-	var st := SurfaceTool.new()
-	st.begin(Mesh.PRIMITIVE_TRIANGLES)
-
+	
+	var vert_cnt: int = size * size
+	var tri_cnt: int = div * div * 6
+	
+	var vertices: PackedVector3Array = PackedVector3Array()
+	vertices.resize(vert_cnt)
+	var uvs: PackedVector2Array = PackedVector2Array()
+	uvs.resize(vert_cnt)
+	var indices: PackedInt32Array = PackedInt32Array()
+	indices.resize(tri_cnt)
+	
+	var idx: int = 0
 	for z_idx in range(size):
 		var vz: float = z_idx * step - half
 		var uv_v: float = z_idx * inv_div * TEX_TILE_SIZE
 		for x_idx in range(size):
 			var vx: float = x_idx * step - half
-			var nv: float = noise.get_noise_2d(vx + chunkX, vz + chunkZ)
-			var vy: float = nv * A
 			var uv_u: float = x_idx * inv_div * TEX_TILE_SIZE
-			st.set_uv(Vector2(uv_u, uv_v))
-			st.add_vertex(Vector3(vx, vy, vz))
-
+			var vy: float = n.get_noise_2d(vx + chunkX, vz + chunkZ) * height_amp
+			vertices[idx] = Vector3(vx, vy, vz)
+			uvs[idx] = Vector2(uv_u, uv_v)
+			idx += 1
+	
+	idx = 0
 	for z_idx in range(div):
+		var base: int = z_idx * size
 		for x_idx in range(div):
-			var i: int = z_idx * (size) + x_idx
-			st.add_index(i)
-			st.add_index(i + 1)
-			st.add_index(i + size)
-			st.add_index(i + 1)
-			st.add_index(i + size + 1)
-			st.add_index(i + size)
+			var i0: int = base + x_idx
+			var i1: int = i0 + 1
+			var i2: int = i0 + size
+			var i3: int = i2 + 1
+			indices[idx + 0] = i0
+			indices[idx + 1] = i1
+			indices[idx + 2] = i2
+			indices[idx + 3] = i1
+			indices[idx + 4] = i3
+			indices[idx + 5] = i2
+			idx += 6
+	
+	var normals: PackedVector3Array = _calculate_normals(vertices, indices)
+	
+	var arrays: Array = []
+	arrays.resize(Mesh.ARRAY_MAX)
+	arrays[Mesh.ARRAY_VERTEX] = vertices
+	arrays[Mesh.ARRAY_TEX_UV] = uvs
+	arrays[Mesh.ARRAY_NORMAL] = normals
+	arrays[Mesh.ARRAY_INDEX] = indices
+	
+	var mesh: ArrayMesh = ArrayMesh.new()
+	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
+	
+	# Generate tangents if a normal-map is present
+	if ground_material is StandardMaterial3D and (ground_material as StandardMaterial3D).normal_texture:
+		mesh.generate_tangents()
+	elif ground_material is ORMMaterial3D and (ground_material as ORMMaterial3D).normal_texture:
+		mesh.generate_tangents()
+	
+	return mesh
 
-	st.generate_normals()
-	st.generate_tangents()
-	return st.commit()
+func _calculate_normals(verts: PackedVector3Array, inds: PackedInt32Array) -> PackedVector3Array:
+	var norms: PackedVector3Array = PackedVector3Array()
+	norms.resize(verts.size())
+	for i in range(0, inds.size(), 3):
+		var i0: int = inds[i]
+		var i1: int = inds[i + 1]
+		var i2: int = inds[i + 2]
+		var n: Vector3 = (verts[i1] - verts[i0]).cross(verts[i2] - verts[i0])
+		if n.length_squared() > 0.0:
+			n = n.normalized()
+		norms[i0] += n
+		norms[i1] += n
+		norms[i2] += n
+	for v in range(norms.size()):
+		norms[v] = norms[v].normalized()
+	return norms
 
 func _apply_mesh(mesh: ArrayMesh) -> void:
 	meshInstance.mesh = mesh
-	meshInstance.create_trimesh_collision()
 	if ground_material:
 		meshInstance.set_surface_override_material(0, ground_material)
+	
+	meshInstance.create_trimesh_collision()
 	collisionShape.shape = mesh.create_trimesh_shape()
-
-func _exit_tree() -> void:
-	if thread and thread.is_alive():
-		thread_mutex.lock()
-		active_threads = max(0, active_threads - 1)
-		thread_mutex.unlock()
-		thread.wait_to_finish()
-		thread = null
